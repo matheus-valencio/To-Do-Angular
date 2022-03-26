425519e8fb7f4cf9165ad9fc9c1e45e60c9c00fa
matheus-valencio <matheus.valencio@live.com>
2022-03-25 22:53:02 -0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,46 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  item;
+  lista: string[];
+  constructor() {
+    this.lista = [];
+  }
+
+  pushLista() {
+    this.lista.push(this.item);
+    localStorage.setItem('tarefa', JSON.stringify(this.lista));
+    console.log(localStorage.getItem('tarefa'));
+  }
+
+  getLista() {
+    return Array.from(
+      localStorage
+        .getItem('tarefa')
+        .replace(/"/g, '')
+        .replace('[', '')
+        .replace(']', '')
+        .split(',')
+    );
+  }
+
+  popLista(i) {
+    localStorage.removeItem(i);
+  }
+
+  allStorage() {
+    var values = [],
+      keys = 'tarefa',
+      i = keys.length;
+
+    while (i--) {
+      values.push(localStorage.getItem(keys[i]));
+    }
+    return values;
+  }
 }
